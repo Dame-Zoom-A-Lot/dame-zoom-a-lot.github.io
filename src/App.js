80329a54126fa9ad4323050b50df0785a96a7b2a
Mantis <mantis@pop-os.mynetworksettings.com>
2024-07-21 21:21:55 -0400

    get the wrong version working todo: nested divs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import HtmlDisplay from './html-display';
 
 function App() {
+  const [rawText, setRawText] = useState()
+  const [userCss, setUserCss] = useState()
+
+
+  const handleMarkdownSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form)
+
+    const formJson = Object.fromEntries(formData.entries())
+    setRawText(formJson.markdown)
+  }
+
+  const handleCssSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form)
+
+    const formJson = Object.fromEntries(formData.entries())
+    setUserCss(formJson.headerCss)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <style>
+        {userCss}
+      </style>
+      <div className="App">
+        <form onSubmit={handleMarkdownSubmit}>
+          <textarea name="markdown" cols="40" rows="40"/>
+          <button type="submit">Convert to HTML</button>
+        </form>
+        <form onSubmit={handleCssSubmit}>
+          <textarea name="headerCss" cols="40" rows="40"/>
+          <button type="submit">Apply Styling</button>
+        </form>
+        <HtmlDisplay textInput={rawText} />
+      </div>
+    </>
+
   );
 }
 
